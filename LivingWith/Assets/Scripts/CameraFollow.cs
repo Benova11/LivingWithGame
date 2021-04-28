5a1b1ebe10215d0a2bfb7f777085da901c6b8d4f
ben <ben@gamejamtech.com>
2021-04-28 20:14:39 +0300

    git init

---
+++ LivingWith/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,111 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+
+    public Transform followTransform;
+    //public BoxCollider2D mapBounds;
+
+   // private float xMin, xMax, yMin, yMax;
+    //private float camY,camX;
+    //private float camOrthsize;
+    //private float cameraRatio;
+    private Camera mainCam;
+    private Vector3 smoothPos;
+    public float smoothSpeed = 0.5f;
+    private bool manualy = false;
+    private float zpos;
+    private float manualZpos;
+    private bool setToManual = false;
+    private bool trigerPoint1 = false;
+    private bool fireCamp = false;
+    private bool cameraMoved = false;
+    [SerializeField] private Transform cameraTrigeer;
+    [SerializeField] private Transform cameraTrigeerEnd;
+    [SerializeField] private Transform fireCampPos;
+
+    private void Start()
+    {
+        //xMin = mapBounds.bounds.min.x;
+        //xMax = mapBounds.bounds.max.x;
+        //yMin = mapBounds.bounds.min.y;
+        //yMax = mapBounds.bounds.max.y;
+        mainCam = GetComponent<Camera>();
+        //StartCoroutine(ExampleCoroutine());
+        //camOrthsize = mainCam.orthographicSize;
+        //cameraRatio = (xMax + camOrthsize) / 2.0f;
+    }
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //camY = Mathf.Clamp(followTransform.position.y, yMin + camOrthsize, yMax - camOrthsize);
+        //camX = Mathf.Clamp(followTransform.position.x, xMin + cameraRatio, xMax - cameraRatio);
+       if (!manualy){
+           zpos = -18;
+        }else if (zpos == -18){
+            zpos = this.transform.position.z - 8;
+        }
+        if (transform.position.x > cameraTrigeer.position.x){
+            manualy = true;
+        }
+        if (transform.position.x > cameraTrigeerEnd.position.x){
+            manualy = false;
+        }
+        if (transform.position.x >= fireCampPos.position.x && !cameraMoved){
+            smoothPos = Vector3.Lerp(this.transform.position, new Vector3(followTransform.transform.position.x + 15, followTransform.transform.position.y + 6, zpos), smoothSpeed - 0.05f);
+            this.transform.position = smoothPos;
+            if (!fireCamp){
+            StartCoroutine(seeFireCamp());
+            fireCamp = true;
+            }
+        }
+
+        if (!fireCamp){
+        smoothPos = Vector3.Lerp(this.transform.position, new Vector3(followTransform.transform.position.x + 4, followTransform.transform.position.y + 6, zpos), smoothSpeed);
+        this.transform.position = smoothPos; 
+        }
+    }
+
+    IEnumerator seeFireCamp()
+    {
+        //Print the time of when the function is first called.
+        Debug.Log("MANUAL" + Time.time);
+
+        //yield on a new YieldInstruction that waits for 5 seconds.
+        //yield return new WaitForSeconds(2);
+        //smoothPos = Vector3.Lerp(this.transform.position, new Vector3(followTransform.transform.position.x + 15, followTransform.transform.position.y + 6, zpos), smoothSpeed - 0.05f);
+        //this.transform.position = smoothPos;
+        yield return new WaitForSeconds(5); 
+        smoothPos = Vector3.Lerp(this.transform.position, new Vector3(followTransform.transform.position.x -15, followTransform.transform.position.y + 6, zpos), smoothSpeed);
+        this.transform.position = smoothPos;
+        fireCamp = false;
+        cameraMoved = true;
+        //manualy = true;
+        //manualZpos = -20;
+        //yield return new WaitForSeconds(5);
+        //manualZpos = -25;
+        //setToManual = false;
+        //this.transform.position = new Vector3()
+
+        //After we have waited 5 seconds print the time again.
+        Debug.Log("Finished Coroutine at timestamp : " + Time.time);
+    }
+
+      IEnumerator ExampleCoroutine()
+    {
+        //Print the time of when the function is first called.
+        Debug.Log("Started Coroutine at timestamp : " + Time.time);
+
+        //yield on a new YieldInstruction that waits for 5 seconds.
+        yield return new WaitForSeconds(5);
+        manualy = true;
+        yield return new WaitForSeconds(5);
+        manualy = false;
+        //this.transform.position = new Vector3()
+
+        //After we have waited 5 seconds print the time again.
+        Debug.Log("Finished Coroutine at timestamp : " + Time.time);
+    }
+}
---
+++ LivingWith/Assets/Scripts/Movement.cs
@@ -0,0 +1,118 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    [Header("Components")]
+    private Rigidbody2D rb;
+
+    [Header("Layer Mask")]
+    [SerializeField] private LayerMask groundLayer;
+
+    [Header("Movement Variables")]
+    [SerializeField] private float movementAcceleration;
+    [SerializeField] private float maxMoveSpeed;
+    [SerializeField] private float groundLinearDrag;
+    private float horizontalDirection;
+    private bool changingDirection => (rb.velocity.x > 0f && horizontalDirection < 0f) || (rb.velocity.x < 0f && horizontalDirection > 0f);
+
+    [Header("Jump Variables")]
+    [SerializeField] private float jumpForce = 12f;
+    [SerializeField] private float airLinearDrag = 2.5f;
+    [SerializeField] private float fallMultiplier = 8f;
+    [SerializeField] private float lowJumpFallMultiplier = 5f;
+    [SerializeField] private int extraJumps = 1;
+    private int extraJumpsValue;
+    private bool canJump => Input.GetButtonDown("Jump") && (onGround || extraJumpsValue > 0);
+    public bool isOnGround => onGround;
+
+    [Header("Ground Collision Variables")]
+    [SerializeField] private float groundRaycastLength;
+    private bool onGround;
+
+
+    private void Start() {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        horizontalDirection = GetInput().x;
+        if (horizontalDirection == 0){
+            rb.velocity = new Vector2(0, rb.velocity.y);
+        }
+        if (canJump) Jump();
+    }
+
+    private void FixedUpdate(){
+        CheckCollisions();
+        MoveGirl();
+        if (onGround){
+            extraJumpsValue = extraJumps;
+            //if (horizontalDirection != 0 ){
+            //ApplyGroundLinearDrag();
+            //}
+        }else{
+            ApplyAirLinearDrag();
+            FallMultiplier();
+        }
+    }
+
+    private Vector2 GetInput()
+    {
+        return new Vector2(Input.GetAxisRaw("Horizontal"),Input.GetAxisRaw("Vertical"));
+    }
+
+    private void MoveGirl()
+    {
+        rb.AddForce(new Vector2(horizontalDirection, 0f) * movementAcceleration);
+
+        if(Mathf.Abs(rb.velocity.x) > maxMoveSpeed){
+            rb.velocity = new Vector2(Mathf.Sign(rb.velocity.x) * maxMoveSpeed, rb.velocity.y);
+        }
+    }
+
+    private void Jump()
+    {
+        Debug.Log("problem");
+        if(!onGround) extraJumpsValue--;
+        rb.velocity = new Vector2(rb.velocity.x , 0f);
+        rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+    }
+
+    private void FallMultiplier()
+    {
+        if(rb.velocity.y < 0){
+            rb.gravityScale = fallMultiplier;
+        }else if(rb.velocity.y > 0 && !Input.GetButtonDown("Jump")){
+            rb.gravityScale = lowJumpFallMultiplier;
+        }else{
+            rb.gravityScale = 1f;
+        }
+    }
+
+    private void ApplyGroundLinearDrag()
+    {
+        if(Mathf.Abs(horizontalDirection) < 0.4f || changingDirection){
+            rb.drag = groundLinearDrag;
+        }else{
+            rb.drag = 0f;
+        }
+    }
+
+    private void ApplyAirLinearDrag()
+    {
+        rb.drag = airLinearDrag;
+    }
+
+    private void CheckCollisions()
+    {
+        onGround = Physics2D.Raycast(transform.position, transform.TransformDirection(Vector3.down),groundRaycastLength,groundLayer);
+    }
+
+    private void OnDrawGizmos() {
+        Gizmos.color = Color.green;
+        Gizmos.DrawLine(transform.position, transform.position + Vector3.down * groundRaycastLength);
+    }
+}
