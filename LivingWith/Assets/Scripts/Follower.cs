8fed54cb2ef9f5b16db3fd6c765fa813e8c90b7e
ben <ben@gamejamtech.com>
2021-05-12 19:29:00 +0300

    follower added

---
+++ LivingWith/Assets/Scripts/Follower.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Follower : MonoBehaviour
+{
+
+    public float speed;
+    public Transform followTransform;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+    private void FixedUpdate() {
+        this.transform.position = Vector3.Lerp(this.transform.position, new Vector3(followTransform.transform.position.x - 3, followTransform.transform.position.y + 0.5f, transform.position.z), speed);
+    }
+    // Update is called once per frame
+}
