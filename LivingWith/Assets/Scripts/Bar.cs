253f6edfe79a6372e607c0ca76cd13d9493aa749
ben <ben@gamejamtech.com>
2021-05-15 20:51:49 +0300

    added flute mech

---
+++ LivingWith/Assets/Scripts/Bar.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Bar : MonoBehaviour
+{
+    float currentBar;
+    public float maxBar = 3.0f;
+    public float deacreseTmp = 0.1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentBar = maxBar;
+        GetComponent<Slider>().maxValue = maxBar;
+        GetComponent<Slider>().value = currentBar;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void decreaseBar()
+    {
+        currentBar -= deacreseTmp;
+        GetComponent<Slider>().value = currentBar;
+
+    }
+
+    public void fillBar(float fillAmount)
+    {
+        if (fillAmount + currentBar <= maxBar)
+        {
+            currentBar += fillAmount;
+        }
+        else
+        {
+            currentBar = maxBar;
+        }
+        GetComponent<Slider>().value = currentBar;
+    }
+
+    public float getBarValue()
+    {
+        return currentBar;
+    }
+}
---
+++ LivingWith/Assets/Scripts/GirlActions.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GirlActions : MonoBehaviour
+{
+    public GameObject flute;
+    Bar bar;
+    private bool isPlayingFlute = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        bar = FindObjectOfType<Bar>();
+        flute.transform.position = new Vector3(transform.position.x + 0.8f, transform.position.y - 0.5f, transform.position.z);
+        flute.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButton("PlayFlute") && (bar.getBarValue() >= 0.0f))
+        {
+            playFlute();
+        }
+        else if (isPlayingFlute)
+        {
+            isPlayingFlute = false;
+            hideFlute();
+        }
+    }
+
+    public void playFlute()
+    {
+        if (bar.getBarValue() > 0)
+        {
+            isPlayingFlute = true;
+            bar.decreaseBar();
+
+            if (!flute.active)
+            {
+                flute.SetActive(true);
+            }
+            
+        }
+    }
+
+    public void hideFlute()
+    {
+        flute.SetActive(false);
+    }
+}
