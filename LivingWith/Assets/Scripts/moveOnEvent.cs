ac156fa795cc64a1493d5b5a4f4bf9ade24b1dee
nisimg <73110726+nisimg@users.noreply.github.com>
2021-05-18 14:12:40 +0300

    PP

--- LivingWith/Assets/Scripts/moveOnEvent.cs
+++ LivingWith/Assets/Scripts/moveOnEvent.cs
@@ -8,6 +8,9 @@ public class moveOnEvent : MonoBehaviour
     Vector3 normalPos;
     [SerializeField]private float speed =0.2f;
     bool IsActive = false;
+    bool IsAtEndPoint = false;
+    bool goBack = false;
+    [SerializeField] Movement Player;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,10 +18,42 @@ public class moveOnEvent : MonoBehaviour
     }
     private void Update()
     {
-       if (IsActive) transform.position = Vector3.MoveTowards(transform.position, Waypoint.position, speed);
+        if (IsActive) 
+        {
+            transform.position = Vector3.MoveTowards(transform.position, Waypoint.position, speed );
+            if (transform.position == Waypoint.position)
+            {
+                IsActive = false;
+                IsAtEndPoint = true;
+                Player.gameObject.transform.SetParent(null);
+            }
+           
+        }
+
+        if (goBack)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, normalPos, speed *0.5F ); 
+        }
+
     }
     public void goToWaypoint()
     {
         IsActive = true;
+        IsAtEndPoint = false;
+        goBack = false;
+
+    }
+
+    public void GoBeckToStartPos( Movement player)
+    {
+        if (IsAtEndPoint) 
+        {
+            IsActive = false;
+            goBack = true;
+            Player = player;
+            Player.gameObject.transform.SetParent(this.gameObject.transform);
+        }
+
+
     }
 }
